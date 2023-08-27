efdbdc6cee82f4de33eb833b355f49095b27744f
Artemon-x58 <artem1145819@gmail.com>
2023-08-27 10:01:57 +0200

    update

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,10 +1,35 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
-export class SearchForm extends Component {
-  render() {
-    return <h2>SearchForm</h2>;
-  }
-}
+export const SearchForm = ({ onSubmit }) => {
+  const [query, setQuery] = useState('');
+  const handleInput = e => {
+    setQuery(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!query.trim()) {
+      alert('Write some text');
+      return;
+    }
+    onSubmit(query);
+  };
+  return (
+    <SearchFormStyled onSubmit={handleSubmit}>
+      <FormBtn type="submit">
+        <FiSearch size="16px" />
+      </FormBtn>
+      <InputSearch
+        onChange={handleInput}
+        value={query}
+        placeholder="What do you want to write?"
+        name="search"
+        required
+        autoFocus
+      />
+    </SearchFormStyled>
+  );
+};
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -1,14 +1,25 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 
 import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
-export class Gallery extends Component {
-  render() {
-    return (
-      <>
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
-      </>
-    );
-  }
-}
+export const Gallery = () => {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    if (!query) return;
+    ImageService.getImages(query, page);
+  }, [query, page]);
+
+  const onSubmit = newQuery => {
+    setQuery(newQuery);
+    console.log(newQuery);
+  };
+  return (
+    <>
+      <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+      <SearchForm onSubmit={onSubmit} />
+    </>
+  );
+};
